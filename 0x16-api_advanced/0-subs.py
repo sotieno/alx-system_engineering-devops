c0c9c2bd5dfb4120df10a1b700a9fc853c355d23
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-05-17 09:22:07 +0000

    Add 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,13 +2,13 @@
 '''Get number of reddit channel subscribers'''
 import requests
 
-BASE_URL = 'http://reddit.com/r/{}/about.json'
+API_URL = 'http://reddit.com/r/{}/about.json'
 
 
 def number_of_subscribers(subreddit):
     '''Gets number of reddit subscribers'''
     headers = {'User-agent': 'Unix:0-subs:v1'}
-    response = requests.get(BASE_URL.format(subreddit),
+    response = requests.get(API_URL.format(subreddit),
                             headers=headers)
     if response.status_code != 200:
         return 0
