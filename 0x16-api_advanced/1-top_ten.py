9ac0b9a0af6f211583197d866f96003b317a217c
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-05-17 09:24:05 +0000

    Add 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Get top 10 hot posts'''
+import requests
+API_URL = 'http://reddit.com/r/{}/hot.json'
+
+
+def top_ten(subreddit):
+    '''Get top 10 hot posts'''
+    headers = {'User-agent': 'Unix:0-subs:v1'}
+    response = requests.get(API_URL.format(subreddit),
+                            headers=headers)
+    data = response.json().get('data', {}).get('children', {})
+    if response.status_code != 200 or not data:
+        return print("None")
+    for post in data[0:10]:
+        print(post.get('data', {}).get('title'))
